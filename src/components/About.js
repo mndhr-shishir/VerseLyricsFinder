8ccd4d1fe0f90e66b3fad497db92053b8f9ad041
lakas422 <shakya.lakash01@gmail.com>
2020-01-25 17:52:21 +0545

    Changed about us

--- src/components/About.js
+++ src/components/About.js
@@ -7,50 +7,13 @@ import parme from '../team/parme.jpg';
 
 const About = () => {
   return (
-    <div className="container">
-      <div className="grid-container">
-        <div className="grid-item card">
-          <div className="card-container">
-            <img alt="" src={shishir} height="50%" width="100%"style={{paddingTop: "2rem"}}/>
-            <h3 className="suggestion-title">Shishir Manandhar</h3>
-            <p className="suggestion-artist">sisir.mdr@gmail.com</p>
-          </div>
-        </div>
-
-        <div className="grid-item card">
-          <div className="card-container">
-            <img alt="" src={anish} height="100%" width="100%" style={{paddingTop: "1.5rem"}}/>
-            <h3 className="suggestion-title">Anish Maharjan</h3>
-            <p className="suggestion-artist">anishmaharjan17@gmail.com</p>
-          </div>
-        </div>
-
-        <div className="grid-item card">
-          <div className="card-container">
-            <img alt="" src={suman} height="50%" width="100%" style={{paddingTop: "1.5rem"}}/>
-            <h3 className="suggestion-title">Suman Marahatha</h3>
-            <p className="suggestion-artist">sumancr.marahata@gmail.com</p>
-          </div>
-        </div>
-
-        <br />
-
-        <div className="grid-item card">
-          <div className="card-container">
-            <img alt="" src={lakash} height="50%" width="100%" style={{paddingTop: "1.5rem"}}/>
-            <h3 className="suggestion-title">Lakash Shakya</h3>
-            <p className="suggestion-artist">shakya.lakash01@gmail.com</p>
-          </div>
-        </div>
-
-        <div className="grid-item card">
-          <div className="card-container">
-            <img alt="" src={parme} height="50%" width="100%" style={{paddingTop: "1.5rem"}}/>
-            <h3 className="suggestion-title">parameshwor rokaha</h3>
-            <p className="suggestion-artist">parameshworokaha@gmail.com</p>
-          </div>
-        </div>
-      </div>
+    <div className="aboutus">
+      <h3>Our Team</h3>
+      <div className=""><p><a href="https://www.github.com/mndhr-shishir">Shishir Manandhar (sisir.mdr@gmail.com)</a></p></div>
+      <div className=""><p><a href="https://www.github.com/MhrjAniiz">Anish Maharjan (anish.maharjan17@gmail.com)</a></p></div>
+      <div className=""><p><a href="https://www.github.com/SumanMarahatha7">Suman Marahatha (sumancr.marahatha@gmail.com)</a></p></div>
+      <div className=""><p><a href="https://www.github.com/lakas422">Lakash Shakya (shakya.lakash01@gmail.com)</a></p></div>
+      <div className=""><p><a href="https://www.github.com/parameshwor">Parameshwor Rokaha (rokahaparameshwor2@gmail.com)</a></p></div>
     </div>
   );
 }
